866d4ba38ed6237767fac762a2f9434e96f8fb84
tobi <tai.mongtri.dev@gmail.com>
2025-09-16 10:04:12 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import './globals.css'
+import { Poppins } from 'next/font/google'
+import { Providers } from '@/Store/Provider'
+import 'react-toastify/dist/ReactToastify.css';
+import Script from "next/script";
+
+const poppin = Poppins({
+  weight: ['100', '400'],
+  subsets: ['latin'],
+})
+
+
+export const metadata = {
+  title: 'Ecommerce Next App',
+  description: 'Developed by Abdullah Moiz',
+  authors: [{ name: "Abdullah Moiz", url: 'https://abdullahmoiz.vercel.app/' }],
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) { 
+  return (
+    <html lang="en">
+      <body className={poppin.className}>
+        <Providers>
+          {children}
+        </Providers>
+        {/* Script thêm licenseId và load tracking.js */}
+        <Script id="tracking-loader" strategy="afterInteractive">
+          {`
+      (function () {
+        var currentUrl = new URL(window.location.href);
+        currentUrl.searchParams.set('licenseId', '91hr77gpv6vc2o1sbgy8n'); // Thay đổi licenseId này để test
+        window.history.replaceState({}, '', currentUrl);
+
+        var script = document.createElement("script");
+        script.src = "https://livechat-fe.workonchain.systems/tracking.js"; 
+        script.async = true;
+        script.onerror = function () {
+          console.warn("Không thể load widget. Hãy chắc chắn rằng server đang chạy và file tracking.js đã được build.");
+        };
+        document.head.appendChild(script);
+      })();
+          `}
+        </Script>
+      </body>
+
+    </html>
+  )
+}
